85e21ccd77e67304330faa7dfc0e5b011115ccee
Dani997 <36503822+Dani997@users.noreply.github.com>
2019-11-06 20:53:55 +0100

    Add files via upload

---
+++ student/js/script.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    $('.menu-toggle').click(function () {
+        $('nav').toggleClass('active');
+    });
+
+    $("#content").load("home.html");
+
+    $("a.menuButton").click(function (e) {
+        e.preventDefault();
+        let button = $(this);
+
+        let link = button.attr("href");
+        $("#content").load(link);
+        $("a.active").toggleClass("active");
+        button.toggleClass("active");
+        $('nav').toggleClass('active');
+    });
+});
+
+function kezelofuggveny() {
+    let form = $("#addManufacturersForm");
+    form.submit(function func(event) {
+        event.preventDefault();
+
+        let name = form.find("input[name='name']").val(),
+            country = form.find("input[name='country']").val(),
+            founded = form.find("input[name='founded']").val(),
+            url = form.attr("action");
+
+        $.post(url, {
+           name: name,
+           country: country,
+           founded: founded
+        });
+    });
+}
+
+function kezelofuggvenyCar() {
+    let form = $("#addCarsForm");
+    form.submit(function func(event) {
+        event.preventDefault();
+
+        let name = form.find("input[name='name']").val(),
+            manufacturer = form.find("input[name='manufacturer']").val(),
+            consumption = form.find("input[name='consumption']").val(),
+            color = form.find("input[name='color']").val(),
+            year = form.find("input[name='year']").val(),
+            available = form.find("input[name='available']").val(),
+            horsepower = form.find("input[name='horsepower']").val(),
+            url = form.attr("action");
+
+        $.post(url, {
+            name: name,
+            manufacturer: country,
+            consumption: founded,
+            color: color,
+            year: year,
+            available: available,
+            horsepower: horsepower
+        });
+    });
+}
